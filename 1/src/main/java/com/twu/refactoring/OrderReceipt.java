4ffa2240eec19f15802978e8b5be836050b14ca5
fgz <1182764581@qq.com>
2021-08-08 23:48:51 +0800

    finish my first 1 hoemwork

--- 1/src/main/java/com/twu/refactoring/OrderReceipt.java
+++ 1/src/main/java/com/twu/refactoring/OrderReceipt.java
@@ -16,42 +16,41 @@ public class OrderReceipt {
 
 	public String printReceipt() {
 		StringBuilder output = new StringBuilder();
-
-		// print headers
-		output.append("======Printing Orders======\n");
-
-		// print date, bill no, customer name
-//        output.append("Date - " + order.getDate();
-        output.append(o.getCustomerName());
-        output.append(o.getCustomerAddress());
-//        output.append(order.getCustomerLoyaltyNumber());
-
-		// prints lineItems
+		output = getHeader(output);
 		double totSalesTx = 0d;
 		double tot = 0d;
 		for (LineItem lineItem : o.getLineItems()) {
-			output.append(lineItem.getDescription());
-			output.append('\t');
-			output.append(lineItem.getPrice());
-			output.append('\t');
-			output.append(lineItem.getQuantity());
-			output.append('\t');
-			output.append(lineItem.totalAmount());
-			output.append('\n');
-
-			// calculate sales tax @ rate of 10%
+			output=getlineItems(output,lineItem);
             double salesTax = lineItem.totalAmount() * .10;
             totSalesTx += salesTax;
-
-            // calculate total amount of lineItem = price * quantity + 10 % sales tax
             tot += lineItem.totalAmount() + salesTax;
 		}
-
-		// prints the state tax
-		output.append("Sales Tax").append('\t').append(totSalesTx);
-
-        // print total amount
-		output.append("Total Amount").append('\t').append(tot);
+		output=getTail(output,totSalesTx,tot);
 		return output.toString();
+	}
+
+	public StringBuilder getHeader(StringBuilder output){
+		output.append("======Printing Orders======\n");
+		output.append(o.getCustomerName());
+		output.append(o.getCustomerAddress());
+		return output;
+	}
+
+	public StringBuilder getlineItems(StringBuilder output,LineItem lineItem){
+		output.append(lineItem.getDescription());
+		output.append('\t');
+		output.append(lineItem.getPrice());
+		output.append('\t');
+		output.append(lineItem.getQuantity());
+		output.append('\t');
+		output.append(lineItem.totalAmount());
+		output.append('\n');
+		return output;
+	}
+
+	public StringBuilder getTail(StringBuilder output,double totSalesTx,double tot){
+		output.append("Sales Tax").append('\t').append(totSalesTx);
+		output.append("Total Amount").append('\t').append(tot);
+		return output;
 	}
 }
